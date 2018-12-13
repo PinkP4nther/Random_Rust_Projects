1a7cec490521e26733945ef808f498f6f35704ee
Pinky <36157015+PinkP4nther@users.noreply.github.com>
2018-12-13 06:06:33 +0000

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,70 @@
+/*
+ * Load in lists (Generate all variants before? Or generate on the fly by pushing extensions
+ * on to String in Vector)
+ * Generate threads of function
+ * Threads grab entries from Arc<Mutex<Vec<String>>>
+ * Or step_by(threadcount) through Arc<Vec<String>> \_ whichever is fastest
+ * Use mpsc to send message to main thread to print HTTP Code & URL
+ * Eventually create data structures for each found entry etc.
+ */
+
+use std::thread;
+use std::sync::{Mutex, Arc};
+use std::time::{Instant, Duration};
+use std::io::{self,Write,BufRead,BufReader};
+use std::fs::File;
+use std::sync::mpsc;
+
+fn main() {
+    let tc: u32 = 10;
+    let wlist: Vec<String> = Vec::new();
+    let wlist_tg = Arc::new(Mutex::new(wlist));
+    let mut thandles = vec![];
+    let (tx, rx) = mpsc::channel();
+
+    for i in 0..tc {
+        let schan = mpsc::Sender::clone(&tx);
+        let wlist_tg = Arc::clone(&wlist_tg);
+        let handle = thread::spawn(move || {
+            work_func(i,wlist_tg,schan);
+        });
+        thandles.push(handle);
+    }
+
+    let mut ti: u32 = 0;
+    for recvd in rx {
+        if ti < 9 {
+            println!("[!] {}",recvd);
+            ti += 1;
+        } else if ti == 9{
+            println!("[!] {}", recvd);
+            break;
+        }
+
+    }
+
+    println!("Iterating through worked on vector!!!");
+    let entries = wlist_tg.lock().unwrap();
+    let mut count = 1;
+    for e in entries.iter() {
+        println!("[{}] {}",count,e);
+        count += 1;
+    }
+}
+
+fn work_func(tn: u32,wlist_tg: Arc<Mutex<Vec<String>>>,chan_send: mpsc::Sender<String>) {
+    let mut c: u32 = 0; 
+    loop {
+        if c < 10 {
+            println!("Hello! from thread {}",tn+1);
+            let mut entry = wlist_tg.lock().unwrap();
+            entry.push(format!("I am {} from thread {}",c+1,tn+1));
+            c += 1;
+        } else {
+            match chan_send.send(format!("Thread {} shutting down!",tn+1)) {
+                Ok(_) => {break},
+                Err(_) => {println!("Receiver not listening! Dying!");return}
+            };
+        }
+    }
+}
